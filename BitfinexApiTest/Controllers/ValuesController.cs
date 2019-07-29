78ea73110ec36c1fb6423c6eaa36fe5006211c3e
Nidhi <tanknidhi@yahoo.com>
2019-07-29 15:56:20 +0530

    .

--- BitfinexApiTest/Controllers/ValuesController.cs
+++ BitfinexApiTest/Controllers/ValuesController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Api1.Utilities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,31 +11,92 @@ namespace BitfinexApiTest.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        string Apikeystr = "kNpeMUgHtn4VZv2cqu18LGuBDflCC3zgks6O9GFBHRL";
+        string Apikeysecret = "6xlqYzLfRoCTiS4WkWIIEY06lnEjPHjnqa0R66tavlN";
+
+        [HttpGet]
+        [Route("GetStatus")]
+        public ApiResponse GetStatus()
         {
-            return new string[] { "value1", "value2" };
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://api-pub.bitfinex.com/v2/platform/status");
+            Request.Accept = "application/vnd.api+json";
+            ApiResponse apiResponse = new ApiResponse();
+            HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+            StreamReader respStreamReader = new StreamReader(response.GetResponseStream());
+            string responseString = respStreamReader.ReadToEnd();
+            respStreamReader.Close();
+            response.Close();
+            apiResponse.Data = responseString;
+            //ContinentsSingle Continent = JsonConvert.DeserializeObject<ContinentsSingle>(responseString);
+            return apiResponse;
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        [HttpGet]
+        [Route("GetAllTicker")]
+        public ApiResponse GetAllTicker()
         {
-            return "value";
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://api-pub.bitfinex.com/v2/tickers?symbols=ALL");
+            Request.Accept = "application/vnd.api+json";
+            ApiResponse apiResponse = new ApiResponse();
+            HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+            StreamReader respStreamReader = new StreamReader(response.GetResponseStream());
+            string responseString = respStreamReader.ReadToEnd();
+            respStreamReader.Close();
+            response.Close();
+            apiResponse.Data = responseString;
+            //ContinentsSingle Continent = JsonConvert.DeserializeObject<ContinentsSingle>(responseString);
+            return apiResponse;
         }
 
-        // POST api/values
-        public void Post([FromBody]string value)
+        [HttpGet]
+        [Route("GetTicker")]
+        public ApiResponse GetTicker(string tickertype)
         {
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://api-pub.bitfinex.com/v2/tickers?symbols=" + tickertype);
+            Request.Accept = "application/vnd.api+json";
+            ApiResponse apiResponse = new ApiResponse();
+            HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+            StreamReader respStreamReader = new StreamReader(response.GetResponseStream());
+            string responseString = respStreamReader.ReadToEnd();
+            respStreamReader.Close();
+            response.Close();
+            apiResponse.Data = responseString;
+            return apiResponse;
         }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        [HttpGet]
+        [Route("Ticker")]
+        public ApiResponse Ticker()
         {
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://api-pub.bitfinex.com/v2/ticker/tBTCUSD");
+            Request.Accept = "application/vnd.api+json";
+            ApiResponse apiResponse = new ApiResponse();
+            HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+            StreamReader respStreamReader = new StreamReader(response.GetResponseStream());
+            string responseString = respStreamReader.ReadToEnd();
+            respStreamReader.Close();
+            response.Close();
+            apiResponse.Data = respStreamReader.ReadToEnd();
+            return apiResponse;
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        [HttpGet]
+        [Route("GetTrade")]
+        public ApiResponse GetTrade()
         {
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://api-pub.bitfinex.com/v2/platform/status");
+            Request.Accept = "application/vnd.api+json";
+            ApiResponse apiResponse = new ApiResponse();
+            HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+            StreamReader respStreamReader = new StreamReader(response.GetResponseStream());
+            string responseString = respStreamReader.ReadToEnd();
+            respStreamReader.Close();
+            response.Close();
+            //apiResponse.Data = responseString;
+            //TradeData Continent = JsonConvert.DeserializeObject<TradeData>(responseString);
+            var Continent = JObject.Parse(responseString);
+            apiResponse.Data = Continent;
+            return apiResponse;
         }
     }
 }
---
+++ BitfinexApiTest/Models/Data.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitfinexApiTest.Models
+{
+    public class Data
+    {
+        public class ContinentsModel
+        {
+            public List<ContinentsData> data { get; set; }
+        }
+
+        public class ContinentsSingle
+        {
+            public ContinentsData data { get; set; }
+        }
+
+        public class ContinentsData
+        {
+            public int OPERATIVE { get; set; }
+        }
+
+        public class TickerData
+        {
+            public float FRR { get; set; }
+            public float BID { get; set; }
+            public int BID_PERIOD { get; set; }
+            public float BID_SIZE { get; set; }
+            public float ASK { get; set; }
+            public int ASK_PERIOD { get; set; }
+            public float ASK_SIZE { get; set; }
+            public float DAILY_CHANGE { get; set; }
+            public float DAILY_CHANGE_PERC { get; set; }
+            public float LAST_PRICE { get; set; }
+            public float VOLUME { get; set; }
+            public float HIGH { get; set; }
+            public float LOW { get; set; }
+        }
+
+        public class TradeData
+        {
+            public int MTS { get; set; }
+            public float AMOUNT { get; set; }
+            public float PRICE { get; set; }
+            public float RATE { get; set; }
+            public int PERIOD { get; set; }
+        }
+    }
+}
